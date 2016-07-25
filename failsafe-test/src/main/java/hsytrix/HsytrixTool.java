152b4643ed49ac85e956b520a0d4daa21156c2e5
even.yw <even.yw@autonavi.com>
2016-07-25 18:04:53 +0800

    新增测试例子

---
+++ failsafe-test/src/main/java/hsytrix/HsytrixTool.java
@@ -0,0 +1,18 @@
+package hsytrix;
+
+public class HsytrixTool {
+    public static void main(String[] args) {
+        
+        try {
+            for (int i = 1; i <=30; i++) {
+                //同步
+                MyCommand command=new MyCommand("group1","command1","thread1");
+                System.out.println(i+command.execute());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ failsafe-test/src/main/java/hsytrix/MyCommand.java
@@ -0,0 +1,87 @@
+package hsytrix;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
+
+public class MyCommand extends HystrixCommand<String> {
+
+    /**
+     * // 熔断器在整个统计时间内是否开启的阀值，默认20秒。也就是10秒钟内至少请求20次，熔断器才发挥起作用  
+        private final HystrixProperty<Integer> circuitBreakerRequestVolumeThreshold;   
+        //熔断器默认工作时间,默认:5秒.熔断器中断请求5秒后会进入半打开状态,放部分流量过去重试  
+        private final HystrixProperty<Integer> circuitBreakerSleepWindowInMilliseconds;   
+        //是否启用熔断器,默认true. 启动  
+        private final HystrixProperty<Boolean> circuitBreakerEnabled;   
+        //默认:50%。当出错率超过50%后熔断器启动.  
+        private final HystrixProperty<Integer> circuitBreakerErrorThresholdPercentage;  
+        //是否强制开启熔断器阻断所有请求,默认:false,不开启  
+        private final HystrixProperty<Boolean> circuitBreakerForceOpen;   
+        //是否允许熔断器忽略错误,默认false, 不开启  
+        private final HystrixProperty<Boolean> circuitBreakerForceClosed;  
+        
+        //使用命令调用隔离方式,默认:采用线程隔离,ExecutionIsolationStrategy.THREAD  
+        private final HystrixProperty<ExecutionIsolationStrategy> executionIsolationStrategy;   
+        //使用线程隔离时，调用超时时间，默认:1秒  
+        private final HystrixProperty<Integer> executionIsolationThreadTimeoutInMilliseconds;   
+        //线程池的key,用于决定命令在哪个线程池执行  
+        private final HystrixProperty<String> executionIsolationThreadPoolKeyOverride;   
+        //使用信号量隔离时，命令调用最大的并发数,默认:10  
+        private final HystrixProperty<Integer> executionIsolationSemaphoreMaxConcurrentRequests;  
+        //使用信号量隔离时，命令fallback(降级)调用最大的并发数,默认:10  
+        private final HystrixProperty<Integer> fallbackIsolationSemaphoreMaxConcurrentRequests;   
+        //是否开启fallback降级策略 默认:true   
+        private final HystrixProperty<Boolean> fallbackEnabled;   
+        // 使用线程隔离时，是否对命令执行超时的线程调用中断（Thread.interrupt()）操作.默认:true  
+        private final HystrixProperty<Boolean> executionIsolationThreadInterruptOnTimeout;   
+        // 统计滚动的时间窗口,默认:5000毫秒circuitBreakerSleepWindowInMilliseconds  
+        private final HystrixProperty<Integer> metricsRollingStatisticalWindowInMilliseconds;  
+        // 统计窗口的Buckets的数量,默认:10个,每秒一个Buckets统计  
+        private final HystrixProperty<Integer> metricsRollingStatisticalWindowBuckets; // number of buckets in the statisticalWindow  
+        //是否开启监控统计功能,默认:true  
+        private final HystrixProperty<Boolean> metricsRollingPercentileEnabled;   
+        // 是否开启请求日志,默认:true  
+        private final HystrixProperty<Boolean> requestLogEnabled;   
+        //是否开启请求缓存,默认:true  
+        private final HystrixProperty<Boolean> requestCacheEnabled; // Whether request caching is enabled.  
+     * @param commandGroup
+     */
+    public MyCommand(String commandGroup,String commandKey,String threadPoolKey) {
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(commandGroup))
+                .andCommandKey(HystrixCommandKey.Factory.asKey(commandKey))
+                .andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey(threadPoolKey))
+                .andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.defaultSetter())
+                /* 配置依赖超时时间,500毫秒*/  
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        .withExecutionTimeoutInMilliseconds(100)
+                        .withCircuitBreakerSleepWindowInMilliseconds(30000)
+                        //.withExecutionIsolationThreadInterruptOnTimeout(false)
+                        
+                        //.withMetricsRollingPercentileWindowBuckets(6)
+                        //.withCircuitBreakerRequestVolumeThreshold(1)
+                        //.withCircuitBreakerErrorThresholdPercentage(51)
+                        //.withRequestCacheEnabled(false)
+                        //.withFallbackEnabled(false)
+                        )
+                
+                );  
+    }
+
+    @Override
+    protected String run() throws Exception {
+        System.out.println("start 同步测试");
+        Thread.sleep(150);
+        // TODO Auto-generated method stub
+        System.out.println("end 同步测试");
+        return "同步测试";
+    }
+    
+    @Override
+    protected String getFallback() {
+        return "执行降级处理";
+    }
+    
+}
